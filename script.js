31eaa91021016b761a57377e0151f5ae335dadb3
hammadkhan333 <144952222+hammadkhan333@users.noreply.github.com>
2024-07-18 17:14:17 +0500

    initial comment

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
-let currentIndex = 0;
+$(document).ready(function() {
 
-function showImage(index) {
-    const gallery = document.querySelector('.gallery');
-    const images = document.querySelectorAll('.gallery-image');
-    const totalImages = images.length;
-    if (index >= totalImages) {
-        currentIndex = 0;
-    } else if (index < 0) {
-        currentIndex = totalImages - 1;
-    } else {
-        currentIndex = index;
-    }
-    gallery.style.transform = `translateX(-${currentIndex * 100}%)`;
-}
-
-function nextImage() {
-    showImage(currentIndex + 1);
-}
-
-function prevImage() {
-    showImage(currentIndex - 1);
-}
-
-// Initialize the gallery by showing the first image
-showImage(currentIndex);
+    $(`[unique-script-id="w-w-dm-id"] .btn-box`).click(function() {
+      $(this).parent().children(".overlay").show();
+  
+    });
+  
+  
+    $(`[unique-script-id="w-w-dm-id"] .close`).click(function() {
+      $(`[unique-script-id="w-w-dm-id"] .overlay`).hide();
+    });
+  });
